d2912b1e580374e1bafa79120746d6bc1a88a489
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-07-11 00:45:56 +0100

    Update

--- script.py
+++ script.py
@@ -5,27 +5,22 @@ from pytube import YouTube
 SAVE_PATH = "home/nacer/project/python" #to_do
 
 # link of the video to be downloaded
-link="https://www.youtube.com/watch?v=xWOoBJUqlbI"
+link = input("Enter your url: ")
 
 
-	# object creation using YouTube
-	# which was imported in the beginning
 yt = YouTube(link)
 
-
-# filters out all the files with "mp4" extension
-mp4files = yt.filter('mp4')
+stream = yt.streams.first()
 
 #to set the name of the file
-yt.set_filename('GeeksforGeeks Video')
 
 # get the video with the extension and
 # resolution passed in the get() function
-d_video = yt.get(mp4files[-1].extension,mp4files[-1].resolution)
 
 try:
 	# downloading the video
-	d_video.download(SAVE_PATH)
+	stream.download(SAVE_PATH)
+
 except:
 	print("Some Error!")
 print('Task Completed!')
