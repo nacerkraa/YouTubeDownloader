19dff7405bff848c6f4d2e2eea09864d6b1fac31
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-07-11 11:44:30 +0100

    Update

--- script0.py
+++ script0.py
@@ -3,9 +3,11 @@ from pytube import Playlist
 p = Playlist('https://www.youtube.com/playlist?list=PLzMcBGfZo4-kCLWnGmK0jUBmGLaJxvi4j')
 
 print(f'Downloading: {p.title}')
-
+i = 0;
 for video in p.videos:
+    i += 1
     print(video.title)
-    st = video.streams.get_highest_resolution()
-    st.download()
+    if i == 10:
+        st = video.streams.get_highest_resolution()
+        st.download()
     #video.streams.first().download()
