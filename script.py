b2e58927b16043ac5c7c19d0109b0ab995034764
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-07-11 01:47:38 +0100

    Update

--- script.py
+++ script.py
@@ -1,8 +1,6 @@
 # importing the module
 from pytube import YouTube
 
-# where to save
-SAVE_PATH = "home/nacer/project/python" #to_do
 
 # link of the video to be downloaded
 link = input("Enter your url: ")
@@ -10,10 +8,11 @@ link = input("Enter your url: ")
 
 yt = YouTube(link)
 
-stream = yt.streams.first()
+yt.streams.filter(only_audio=True)
+stream = yt.streams.get_highest_resolution()
 
 try:
-	stream.download(SAVE_PATH)
+	stream.download("Home/Downloads")
 except:
 	print("Some Error!")
 print('Task Completed!')
