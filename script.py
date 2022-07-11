03d151e4d0a6f829a80deafcd13086005d8ddd79
nacerkraa <nacer.kraa@univ-constantine2.dz>
2022-07-11 02:06:18 +0100

    Update

--- script.py
+++ script.py
@@ -5,12 +5,14 @@ from pytube import YouTube
 # link of the video to be downloaded
 link = input("Enter your url: ")
 
+typeVideo = input("Type (y) for audio: ")
 
 yt = YouTube(link)
 
-stream = yt.streams.get_audio_only()
-# stream = yt.streams.get_highest_resolution()
-
+if typeVideo == "y":
+	stream = yt.streams.get_audio_only()
+else:
+	stream = yt.streams.get_highest_resolution()
 try:
 	stream.download("Home/Downloads")
 except:
